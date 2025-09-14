1863cdaad9e11048f60d84fcec29e53c0c3d5083
asliddinx278-ops <asliddinx278@gmail.com>
2025-09-14 21:36:28 +0500

    Update main.js

--- modules/main.js
+++ modules/main.js
@@ -4,41 +4,36 @@
 
 // 1. Rasmlar – **imgur** CDN (o‘zingiznikiga almashtiring)
 
-const IMAGES = {
-  osh:      'https://i.ibb.co/tTY9cFfS/image1.jpg',  // Osh rasmi
-  shorva:   'https://i.ibb.co/PZtGJQDf/183ce0-3a3bcf40ab8d4a1abeb6d62d30576bd1-mv2.jpg', // Sho'rva
-  kabob:    'https://i.ibb.co/PGv4rR6t/Beef-Shish-Kabob-6.jpg', // Kabob
-  salat:    'https://i.ibb.co/NgsxmcmP/instagram1080-1024x1024.png', // Salat
-  ichimlik: 'https://i.ibb.co/Z1gqqvrc/original.jpg', // Ichimlik
-  desert:   'https://i.ibb.co/9myWgVZb/bfdejixlfomrkzsdmxsq-ogd2pemlkxf.webp' // Desert
-};
-
 export const FULL_MENU = {
   osh: [
-    {id:1, name:"G‘ijduvoncha osh",   price:25000, img: IMAGES.osh},
-    {id:2, name:"Farg‘ona osh",       price:27000, img: IMAGES.osh},
-    {id:3, name:"Toshkentcha osh",    price:26000, img: IMAGES.osh},
-    {id:4, name:"Qovurma osh",        price:28000, img: IMAGES.osh},
-    {id:5, name:"Toy osh",            price:35000, img: IMAGES.osh}
+    {id:1, name:"G‘ijduvoncha osh",   price:25000, img: "https://i.ibb.co/.../gijduvon.jpg"},
+    {id:2, name:"Farg‘ona osh",       price:27000, img: "https://i.ibb.co/.../fargona.jpg"},
+    {id:3, name:"Toshkentcha osh",    price:26000, img: "https://i.ibb.co/.../toshkent.jpg"},
+    {id:4, name:"Qovurma osh",        price:28000, img: "https://i.ibb.co/.../qovurma.jpg"},
+    {id:5, name:"Toy osh",            price:35000, img: "https://i.ibb.co/.../toy.jpg"}
   ],
   shorva: [
-    {id:6, name:"Mastava",        price:12000, img: IMAGES.shorva},
-    {id:7, name:"Sho‘rva",        price:13000, img: IMAGES.shorva},
-    {id:8, name:"Moshxo‘rda",     price:14000, img: IMAGES.shorva}
+    {id:6, name:"Mastava",        price:12000, img: "https://i.ibb.co/.../mastava.jpg"},
+    {id:7, name:"Sho‘rva",        price:13000, img: "https://i.ibb.co/.../shorva.jpg"},
+    {id:8, name:"Moshxo‘rda",     price:14000, img: "https://i.ibb.co/.../moshxor.jpg"}
   ],
   kabob: [
-    {id:9,  name:"Jigar kabob",  price:18000, img: IMAGES.kabob},
-    {id:10, name:"Go‘sht kabob", price:20000, img: IMAGES.kabob}
+    {id:9,  name:"Jigar kabob",   price:18000, img: "https://i.ibb.co/.../jigar.jpg"},
+    {id:10, name:"Go‘sht kabob",  price:20000, img: "https://i.ibb.co/.../gosht.jpg"}
   ],
   salat: [
-    {id:11, name:"Chimchi", price:6000, img: IMAGES.salat},
-    {id:12, name:"Smak",    price:7000, img: IMAGES.salat}
+    {id:11, name:"Chimchi", price:6000, img: "https://i.ibb.co/.../chimchi.jpg"},
+    {id:12, name:"Smak",   price:7000, img: "https://i.ibb.co/.../smak.jpg"}
   ],
   ichimlik: [
-    {id:13, name:"Coca-Cola 0.5", price:6000, img: IMAGES.ichimlik}
+    {id:13, name:"Coca-Cola 0.5", price:6000, img: "https://i.ibb.co/.../cola.jpg"},
+    {id:14, name:"Fanta 0.5",     price:6000, img: "https://i.ibb.co/.../fanta.jpg"},
+    {id:15, name:"Pepsi 0.5",     price:6000, img: "https://i.ibb.co/.../pepsi.jpg"}
   ],
   desert: [
-    {id:14, name:"Shakarap", price:4000, img: IMAGES.desert}
+    {id:16, name:"Shakarap", price:4000, img: "https://i.ibb.co/.../shakarap.jpg"},
+    {id:17, name:"Pahlava",  price:7000, img: "https://i.ibb.co/.../pahlava.jpg"},
+    {id:18, name:"Medovik",  price:8000, img: "https://i.ibb.co/.../medovik.jpg"}
   ]
 };
 
