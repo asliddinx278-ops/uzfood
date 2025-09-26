2a43165127a941742bd077d4a8f7d37275e2ca6a
asliddinx278-ops <asliddinx278@gmail.com>
2025-09-26 19:22:54 +0500

    Update wishlist.js

--- wishlist.js
+++ wishlist.js
@@ -1,3 +1,5 @@
+import { FULL_MENU } from './main.js';
+
 export function renderWishlist() {
   const likes = JSON.parse(localStorage.getItem('wish') || '[]');
   const container = document.getElementById('wishList');
@@ -6,16 +8,19 @@ export function renderWishlist() {
     return;
   }
   container.innerHTML = '';
-  Object.values(FULL_MENU).flat().filter(i => likes.includes(i.id)).forEach(item => {
-    const card = document.createElement('div');
-    card.className = 'menu-card';
-    card.innerHTML = `
-      <img src="${item.img}" alt="${item.name}">
-      <div class="menu-info">
-        <h3>${item.name}</h3>
-        <p class="price">${item.price.toLocaleString()} so‘m</p>
-        <button class="add-btn" onclick="addToCart(${item.id},'${item.name}',${item.price},1,'${item.img}')">Tanlash</button>
-      </div>`;
-    container.appendChild(card);
-  });
+  Object.values(FULL_MENU).flat()
+    .filter(i => likes.includes(i.id))
+    .forEach(item => {
+      const card = document.createElement('div');
+      card.className = 'menu-card';
+      card.innerHTML = `
+        <img src="${item.img}" alt="${item.name}">
+        <div class="menu-info">
+          <h3>${item.name}</h3>
+          <div class="rate">⭐ ${item.rating}</div>
+          <p class="price">${item.price.toLocaleString()} so‘m</p>
+          <button class="add-btn" onclick="addToCart(${item.id},'${item.name}',${item.price},1,'${item.img}')">Tanlash</button>
+        </div>`;
+      container.appendChild(card);
+    });
 }
